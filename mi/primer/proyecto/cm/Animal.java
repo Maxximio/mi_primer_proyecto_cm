03b945c91aae0072e5a5baf5dace5d3e30948e7e
LENOVO LEGION <LENOVO LEGION@LAPTOP-6HI01QD9>
2021-11-29 20:05:08 -0500

    El primer cambio realizado en clase con Github

---
+++ mi/primer/proyecto/cm/Animal.java
@@ -0,0 +1,11 @@
+package mi.primer.proyecto.cm;
+
+public class Animal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("mi primer proyecto en github");
+	}
+
+}
