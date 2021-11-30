19b19b6fa51bb7dffcacf11200a3b0dd2dc8d753
LENOVO LEGION <LENOVO LEGION@LAPTOP-6HI01QD9>
2021-11-29 20:14:45 -0500

    tercero

--- mi/primer/proyecto/cm/Animal.java
+++ mi/primer/proyecto/cm/Animal.java
@@ -5,7 +5,6 @@ public class Animal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("mi primer proyecto en github");
-		System.out.println("push pushhh");
 	}
 
 }
