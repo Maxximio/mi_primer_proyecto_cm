d17a3303e30925e2705d107727943ec770efa4c1
LENOVO LEGION <LENOVO LEGION@LAPTOP-6HI01QD9>
2021-12-13 19:16:25 -0500

    taller 6

---
+++ com/ec/inyeccion/dependencias/framework/Cuenta.java
@@ -0,0 +1,23 @@
+
+public class Cuenta {
+	private String numero;
+	private int Saldo;
+	
+	//get y set
+	
+	public String getNumero() {
+		return numero;
+	}
+	
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	
+	public int getSaldo() {
+		return saldo;
+	}
+	
+	public void setSaldo(int saldo) {
+		this.saldo = saldo;
+	}
+}
